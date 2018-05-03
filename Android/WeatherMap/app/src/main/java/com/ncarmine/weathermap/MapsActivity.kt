dc9c79e19657c7be1f629dee4fe3488027ba782d
ncarmine <nathanacarmine@gmail.com>
2018-05-02 23:29:33 -0600

    :sparkles: Add time-based markers

--- Android/WeatherMap/app/src/main/java/com/ncarmine/weathermap/MapsActivity.kt
+++ Android/WeatherMap/app/src/main/java/com/ncarmine/weathermap/MapsActivity.kt
@@ -15,22 +15,50 @@ import java.net.URL
 import kotlinx.serialization.*
 import kotlinx.serialization.json.JSON
 import android.os.StrictMode
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageButton
 import com.google.android.gms.maps.model.Marker
 import kotlin.math.abs
 
 
-// Serializable Weather and CurrentData data classes to put json data into
+// Serializable Weather and HourData data classes to put json data into
 @Serializable
-data class Weather(val timezone: String, val latitude: Double, val longitude: Double, val currently: CurrentData)
+data class Weather(val timezone: String, val latitude: Double, val longitude: Double,
+                   val currently: HourData, val hourly: HourWeather, val daily: DayWeather)
 
 @Serializable
-data class CurrentData(val time: Long, val temperature: Float, val apparentTemperature: Float, val humidity: Float, val precipProbability: Float)
+data class HourWeather(val summary: String, val icon: String, val data: Array<HourData>)
+
+@Serializable
+data class DayWeather(val summary: String, val icon: String, val data: Array<DayData>)
+
+@Serializable
+data class HourData(val time: Long, val temperature: Float, val apparentTemperature: Float,
+                    val humidity: Float, val precipProbability: Float)
+
+@Serializable
+data class DayData(val time: Long, val temperatureHigh: Float, val temperatureLow: Float,
+                   val apparentTemperatureHigh: Float, val apparentTemperatureLow: Float,
+                   val humidity: Float, val precipProbability: Float)
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private var markers = arrayOf<Marker>()
+    private var weatherOnMap = arrayOf<Weather>()
+    // HashMap to grab index of where weather is in data
+    private val timeMap = hashMapOf(
+            "1h" to 1,
+            "2h" to 2,
+            "3h" to 3,
+            "6h" to 4,
+            "12h" to 5,
+            "1d" to 1,
+            "2d" to 2,
+            "3d" to 3
+    )
+    private var timeSelected = "Now"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,8 +98,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             runOnUiThread {
                 // Serialize JSON for easy referencing
                 val weather = JSON.nonstrict.parse<Weather>(result)
-                // Add a marker at the location
-                markers += mMap.addMarker(MarkerOptions().position(LatLng(weather.latitude, weather.longitude)).title(weather.currently.temperature.toString()))
+                weatherOnMap += weather // add weather to weatherOnMap array
+                // Get the index of where to grab time from based off time hashmap
+                val timeIndex = timeMap[timeSelected]
+                // Add a marker at the location for the weather based off the appropriate time slot
+                when (timeSelected) {
+                    "Now" -> markers += mMap.addMarker(MarkerOptions().position(LatLng(weather.latitude, weather.longitude)).title(weather.currently.temperature.toString()))
+                    "1h", "2h", "3h", "6h", "12h" -> { // Hourly
+                        if (timeIndex != null) {
+                            markers += mMap.addMarker(MarkerOptions().position(LatLng(weather.latitude, weather.longitude)).title(weather.hourly.data[timeIndex].temperature.toString()))
+                        }
+                    }
+                    "1d", "2d", "3d" -> { // Daily
+                        if (timeIndex != null) {
+                            markers += mMap.addMarker(MarkerOptions().position(LatLng(weather.latitude, weather.longitude)).title(weather.daily.data[timeIndex].temperatureHigh.toString()))
+                        }
+                    }
+                }
             }
         }
     }
@@ -87,9 +130,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     marker.remove()
                 }
                 markers = emptyArray() // Empty marker array
+                weatherOnMap = emptyArray() // Empty weather array
                 // Get the region of the current map and center
                 val region = mMap.projection.visibleRegion.latLngBounds
                 val center = region.center
+                // Calculate the four cross-sections to put markers based off average of center and respective corners
                 val top = center.latitude + abs((center.latitude - region.northeast.latitude)/2)
                 val bottom = center.latitude - abs((center.latitude - region.southwest.latitude)/2)
                 val left = center.longitude - abs((center.longitude - region.southwest.longitude)/2)
@@ -102,5 +147,44 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 addMarkerFromJSON(LatLng(bottom, left))
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.time_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        timeSelected = item.toString()
+        // Get the index of where to grab time from based off time hashmap
+        val timeIndex = timeMap[timeSelected]
+        // Get the weather for the appropriate time slot
+        when (timeSelected) {
+            "Now" -> {
+                for ((marker, weather) in markers.zip(weatherOnMap)) {
+                    marker.title = weather.currently.temperature.toString()
+                }
+            }
+            "1h", "2h", "3h", "6h", "12h" -> { // Hourly
+                for ((marker, weather) in markers.zip(weatherOnMap)) {
+                    if (timeIndex != null) {
+                        marker.title = weather.hourly.data[timeIndex].temperature.toString()
+                    } else {
+                        marker.title = "Error"
+                    }
+                }
+            }
+            "1d", "2d", "3d" -> { // Daily
+                for ((marker, weather) in markers.zip(weatherOnMap)) {
+                    if (timeIndex != null) {
+                        marker.title = weather.daily.data[timeIndex].temperatureHigh.toString()
+                    } else {
+                        marker.title = "Error"
+                    }
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
